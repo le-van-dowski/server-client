e2bad9fc1dd3ef29b1356e411e5c21440cf2910a
le-van-dowski <aksperdelepw@gmail.com>
2025-10-06 09:27:56 +0200

    server e socket

--- src/main/java/it/terenzi/Main.java
+++ src/main/java/it/terenzi/Main.java
@@ -1,7 +1,15 @@
 package it.terenzi;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Hello world!");
+        ServerSocket ss = new ServerSocket(3000);
+        Socket mySocket = ss.accept();//istruzione bloccante fino a un collegamento alla port
+        System.out.println("client si è connesso");
+
     }
 }
