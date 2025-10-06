4ff67c62edc2a45a819b02eb47aa8c9279051037
le-van-dowski <aksperdelepw@gmail.com>
2025-10-06 10:00:32 +0200

    dialogo server

--- src/main/java/it/terenzi/Main.java
+++ src/main/java/it/terenzi/Main.java
@@ -1,15 +1,25 @@
 package it.terenzi;
 
-import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception{
         System.out.println("Hello world!");
         ServerSocket ss = new ServerSocket(3000);
         Socket mySocket = ss.accept();//istruzione bloccante fino a un collegamento alla port
-        System.out.println("client si è connesso");
+        System.out.println("connesso");
+        
+        BufferedReader in =new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        PrintWriter out =new PrintWriter(mySocket.getOutputStream(), true);
 
+        out.println("welcome | v1.0.0");
+        String str =in.readLine();
+        str = str.toUpperCase();
+        out.println(str);
     }
 }
